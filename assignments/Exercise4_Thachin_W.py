01ca000cdaa4ff61cc1c327873db069abdb710e7
nickethachin <34716680+nickethachin@users.noreply.github.com>
2020-03-06 17:56:37 +0700

    Add Exercise 4

---
+++ assignments/Exercise4_Thachin_W.py
@@ -0,0 +1,9 @@
+subjFE = 80.0
+subjGB = 80.0
+subjCS = 80.0
+subjCP = 80.0
+print("--- Your Score ---")
+print("Foundation English", subjFE)
+print("General Business", subjGB)
+print("Introduction to Computer Systems", subjCS)
+print("Computer Programming", subjCP)
